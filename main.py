e00459809b49516e460dfb7bcd8d9e809aed8799
mattertater <mmcdade9@gmail.com>
2018-04-09 18:17:19 -0400

    potential fix

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-import logging, datetime, schedule, time
+import telegram, logging, datetime, schedule, time
 
+bot = telegram.bot(token='568352615:AAEGpAfoFY1994WDGQRZm7TcHDNeP0WU8_M')
 updater = Updater(token='568352615:AAEGpAfoFY1994WDGQRZm7TcHDNeP0WU8_M')
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def after_dark(bot, update):
-    bot.set_chat_title(chat_id=update.message.chat_id, title=u"Tildé After Dark")
+def after_dark():
+    bot.set_chat_title(chat_id='-228400508', title=u"Tildé After Dark")
 
-def good_morning(bot, update):
-    bot.set_chat_title(chat_id=update.message.chat_id, title=u"Tildé")
+def good_morning():
+    bot.set_chat_title(chat_id='-228400508', title=u"Tildé")
 
 def hey(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="hey")
-    schedule.every().day.at("00:00").do(after_dark(bot, update))
-    schedule.every().day.at("07:00").do(good_morning(bot, update))
+
+schedule.every().day.at("00:00").do(after_dark(), "midnight name change")
+schedule.every().day.at("07:00").do(good_morning(), "morning name change")
 
 start_handler = CommandHandler('hey', hey)
 dispatcher.add_handler(start_handler)
